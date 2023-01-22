263604128d47d5601cd2ae36fa5ad7380a0b4553
gunjvm <gunjvm@users.noreply.github.com>
2023-01-21 22:50:48 -0500

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,13 +1,9 @@
 def call(body) {
  
   echo "Start Sonar Scans"
-  environment {
-      scannerHome = tool 'ibt-sonarqube';
-      }
-   steps {
-       sh 'echo performing sonar scans'
-       withSonarQubeEnv(credentialsId: 'SQ-student', installationName: 'IBT sonarqube') {
-       sh "${scannerHome}/bin/sonar-scanner"
-        }
-       }
+  def scannerHome = tool 'ibt-sonarqube';
+  
+  withSonarQubeEnv(credentialsId: 'SQ-student', installationName: 'IBT sonarqube') {
+    sh "${scannerHome}/bin/sonar-scanner"
+  }
 }
